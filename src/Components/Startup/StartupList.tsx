6e735fd785cbd35ddb0be5b6f08c144b89c368f2
Yousaf <yousafishaq13@gmail.com>
2022-05-04 16:01:06 +0200

    updated service and compoent

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,50 @@
-import { Fragment, ReactElement } from "react";
+import {Card, CardContent, CardHeader, Grid} from "@mui/material";
+import {ReactElement, useEffect, useState} from "react";
+import {StartupHttpService} from "../../Http/Startup/Startup.http.service";
+import {Startup} from "../../Types/Startup";
 
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+  const [startups, setStartups] = useState<Startup[]>([]);
+
+  useEffect(() => {
+    StartupHttpService.getStartups().then((startups) => {
+      return setStartups(startups);
+    });
+  }, []);
+  return <Grid id="startup-list" container direction="row" spacing={2}>
+      {startups.map((startup) => {
+          return (
+              <Grid
+                  item
+                  xs={12}
+                  key={startup.id}
+                  data-cy={"startupId-" + startup.id}
+              >
+                  <Card>
+                    <CardHeader
+                        title={startup.name}
+                        subheader={`Founded: ${startup.dateFounded.getFullYear()}
+                    ${
+                            startup.employees
+                                ? " | " + startup.employees + " Employees"
+                                : ""
+                        }  
+                    ${
+                            startup.totalFunding?
+                                 " | " + startup.totalFunding + " Mio. $" : " | Funding Undisclosed"
+                        } 
+                     ${
+                            startup.currentInvestmentStage
+                                ? " | " + startup.currentInvestmentStage
+                                : ""
+                        }`}
+                    />
+                    <CardContent sx={{ pt: 0 }}>
+                      {startup.shortDescription}
+                    </CardContent>
+                  </Card>
+              </Grid>
+          );
+        })}
+</Grid>
 }
--- src/Http/Startup/Startup.http.service.ts
+++ src/Http/Startup/Startup.http.service.ts
@@ -7,4 +7,8 @@ export class StartupHttpService {
     const response = await axios.get<StartupDTO>(`/api/startups/${id}`);
     return StartupMapper.map(response.data);
   }
+
+  public static async getStartups(): Promise<Startup[]> {
+    const response = await axios.get<StartupDTO[]>(`/api/startups`, {params: {all: true}});
+  }
 }
